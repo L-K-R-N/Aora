4c3e5241f02b6e5fe5c2fdc717f7b837d713b429
L K R N <artemabramcuk45@gmail.com>
2024-10-21 00:49:46 +0300

    add start, signin and signup screens

---
+++ components/FormField.tsx
@@ -0,0 +1,61 @@
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+
+import { icons } from "../constants";
+
+interface FormFieldProps {
+  title: string;
+  value: string;
+  placeholder?: string;
+  otherStyles: string;
+  handleChangeText: (text: string) => void;
+}
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}: FormFieldProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
+      <View className="border-2 border-black-200 focus:border-secondary w-full h-16 px-4 bg-black-100 rounded-2xl items-center flex-row">
+        <TextInput
+          className="flex-1 text-white  font-psemibold text-base "
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+        />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
+
+const styles = StyleSheet.create({});
